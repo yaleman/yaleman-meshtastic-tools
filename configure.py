f609c9c233057a1da38756da585c3e5a45ce866f
James Hodgkinson <james@terminaloutcomes.com>
2024-05-03 11:32:49 +1000

    updating bluetooth config too

--- configure.py
+++ configure.py
@@ -1,3 +1,4 @@
+import json
 from typing import Optional
 
 from meshtastic.serial_interface import SerialInterface
@@ -19,8 +20,15 @@ class MqttConfig(BaseModel):
     mapReportingEnabled: bool = False
 
 
+class BluetoothConfig(BaseModel):
+    enabled: bool = False
+    fixedPin: int = 12355
+    # mode: str = "RANDOM_PIN"
+
+
 class Config(BaseModel):
     mqtt: Optional[MqttConfig] = None
+    bluetooth: Optional[BluetoothConfig] = None
 
 
 def main():
@@ -37,27 +45,51 @@ def main():
         return
 
     logger.info("Device name: {}", client.getLongName())
-    logger.info("Device info:")
-    logger.info(client.myInfo)
+    logger.info("Device info:\n{}", client.myInfo)
 
-    my_node_num = client.myInfo.my_node_num
-    node = client.getNode(my_node_num)
-    print(node.getMetadata())
+    # my_node_num = client.myInfo.my_node_num
+    # node = client.getNode(my_node_num)
+    node = client.localNode
+    # logger.info("printing metadata...\n{}", node.getMetadata())
+
+    logger.debug("Waiting for config...")
+    node.waitForConfig()
 
     need_to_write_mqtt = False
-    for key, value in config["mqtt"].items():
+    for key, value in config.mqtt.model_dump().items():
         key = camel_to_snake(key)
         if not hasattr(node.moduleConfig.mqtt, key):
-            print(f"mqtt has no attribute {key}")
+            logger.debug(f"mqtt has no attribute {key}")
             continue
         oldvalue = getattr(node.moduleConfig.mqtt, key)
         if oldvalue != value:
             setattr(node.moduleConfig.mqtt, key, value)
-            print(f"updating mqtt {key} from {oldvalue} to {value}")
+            logger.info(f"updating mqtt {key} from {oldvalue} to {value}")
             need_to_write_mqtt = True
     if need_to_write_mqtt:
-        print("writing mqtt config")
+        logger.info("writing mqtt config")
+        logger.debug(json.dumps(node.moduleConfig.mqtt, default=str, indent=4))
         node.writeConfig("mqtt")
+
+        logger.debug("Waiting for reboot...")
+        node.waitForConfig()
+
+    need_to_write_bluetooth = False
+    for key, value in config.bluetooth.model_dump().items():
+        key = camel_to_snake(key)
+        if not hasattr(node.localConfig.bluetooth, key):
+            logger.debug(f"bluetooth has no attribute {key}")
+            continue
+        oldvalue = getattr(node.localConfig.bluetooth, key)
+        if oldvalue != value:
+            setattr(node.localConfig.bluetooth, key, value)
+            logger.info(f"updating bluetooth {key} from {oldvalue} to {value}")
+            need_to_write_bluetooth = True
+    if need_to_write_bluetooth:
+        logger.info("writing bluetooth config")
+        node.writeConfig("bluetooth")
+        logger.debug("Waiting for reboot...")
+        node.waitForConfig()
 
 
 if __name__ == "__main__":
