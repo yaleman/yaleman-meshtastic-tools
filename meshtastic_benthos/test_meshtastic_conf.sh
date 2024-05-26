37f0c66256e13dfbfae156424836b040b27056e6
James Hodgkinson <james@terminaloutcomes.com>
2024-05-26 11:39:06 +1000

    adding auto test

--- meshtastic_benthos/test_meshtastic_conf.sh
+++ meshtastic_benthos/test_meshtastic_conf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+set -e
+
 if [ "$(basename "$(pwd)")" != "meshtastic_benthos" ]; then
     echo "Run this script from the same dir"
     exit 1
 fi
 
-docker run \
-    --mount "type=bind,src=$(pwd),target=/conf" \
-    --rm ghcr.io/benthosdev/benthos -c /conf/benthos.yaml test /conf/...
+benthos -c "$(pwd)/benthos.yaml" test "$(pwd)/..."
