05a08fe99ee6b6b9b7b44e51ca8806494e55091d
James Hodgkinson <james@terminaloutcomes.com>
2024-06-18 10:37:31 +1000

    no tty

--- meshtastic_benthos/test_meshtastic_conf.sh
+++ meshtastic_benthos/test_meshtastic_conf.sh
@@ -6,7 +6,7 @@ if [ "$(basename "$(pwd)")" != "meshtastic_benthos" ]; then
     echo "Run this script from the same dir"
     exit 1
 fi
-docker run --rm -it --mount "type=bind,src=$(pwd),target=/data/" \
+docker run --rm -i --mount "type=bind,src=$(pwd),target=/data/" \
     -e "CONF_DIR=/data/" \
     ghcr.io/redpanda-data/connect:4.30.1 \
     -c /data/benthos.yaml test /data/*.yaml
